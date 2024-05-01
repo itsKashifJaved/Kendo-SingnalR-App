209c18fcdff42697207a869fd09968bd56dc5ba6
Kashif Javed <kashifjaved03@gmail.com>
2024-05-01 01:36:41 -0700

    Initial Source Commit

---
+++ KendoUISignalRApp/Hubs/ProductHub.cs
@@ -0,0 +1,152 @@
+﻿using Kendo.DynamicLinq;
+using KendoUISignalRApp.Models;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Dynamic;
+using Task = System.Threading.Tasks.Task;
+
+namespace KendoUISignalRApp.Hubs
+
+{
+	public class ProductHub : Hub
+    {
+        private readonly ProductService _productService;
+        private static readonly Dictionary<string, SignalRClient> HubClients = new Dictionary<string, SignalRClient>();
+
+        public ProductHub()
+        { 
+            _productService = new ProductService(new SampleDataContext());
+        }
+
+        public DataSourceResult Read(KendoDataSourceRequest request)
+        {
+            try
+            {
+                CurrentClient().KendoDataSourceRequest = request;
+
+                var products = _productService.Read();
+
+                if (request.Sort == null || !request.Sort.Any())    // Add default sorting
+                {
+                    products = products.OrderBy(p => p.ProductName);
+                }
+
+                var q = products.AsQueryable();
+                var retval = products.ToDataSourceResult(request.Take, request.Skip, request.Sort, request.Filter, request.Aggregates);
+                return retval;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+        public void Update(ProductViewModel product)
+        {
+            _productService.Update(product);
+
+            var hub = GlobalHost.ConnectionManager.GetHubContext<ProductHub>();
+            var tempProdlist = new List<ProductViewModel> {product};
+
+            foreach (var client in HubClients.Values)
+            {
+                // Mimic behaviour of Clients.Others
+                // Continue the foreach when client is itself
+                if (client.ConnectionID == Context.ConnectionId) continue;
+
+                var request = client.KendoDataSourceRequest;
+                var result = tempProdlist.AsQueryable().ToDataSourceResult(request.Take, request.Skip, request.Sort, request.Filter, request.Aggregates);
+
+                // Only send data if (filtered and paged) result holds data
+                if (result.Data.Any())
+                {
+                    hub.Clients.Client(client.ConnectionID).update(result);
+                }
+                else
+                {
+                    hub.Clients.Client(client.ConnectionID).destroy(new DataSourceResult { Data = new[] { product } });  // Remove results that are not valid for this filter
+                }
+            }
+        }
+
+        public void Destroy(ProductViewModel product)
+        {
+            _productService.Destroy(product);
+            Clients.Others.destroy(new DataSourceResult { Data = new[] { product } });
+        }
+
+        public DataSourceResult Create(ProductViewModel product)
+        {
+            _productService.Create(product);
+
+            var dataSourceResult = new DataSourceResult {Data = new[] {product}}; // For this client
+
+            var hub = GlobalHost.ConnectionManager.GetHubContext<ProductHub>();
+            var tempProdlist = new List<ProductViewModel> { product };
+
+            foreach (var client in HubClients.Values)
+            {
+                // Mimic behaviour of Clients.Others
+                // Continue the foreach when client is itself
+                if (client.ConnectionID == Context.ConnectionId) continue;
+
+                var request = client.KendoDataSourceRequest;
+                var result = tempProdlist.AsQueryable().ToDataSourceResult(request.Take, request.Skip, request.Sort, request.Filter, request.Aggregates);
+
+                // Only send data if (filtered and paged) result holds data
+                if (result.Data.Any())
+                {
+                    hub.Clients.Client(client.ConnectionID).create(result);
+                }
+            }
+
+            Clients.Others.showMessage($"{DateTime.Now} Message from server: Create product {product.ProductID}");
+
+            return dataSourceResult;
+        }
+        
+        private SignalRClient CurrentClient()
+        {
+            return HubClients[Context.ConnectionId];
+        }
+
+        private void AddClient()
+        {
+            // Create a new client object for this context
+            var client = new SignalRClient(Context.ConnectionId);
+            HubClients.Add(client.ConnectionID, client);
+        }
+
+        private void RemoveClient()
+        {
+            // Remove client object from collection
+            if (HubClients.ContainsKey(Context.ConnectionId))
+            {
+                HubClients.Remove(Context.ConnectionId);
+            }
+        }
+        public override Task OnConnected()
+        {
+            Debug.WriteLine($"Connected: Client = {Context.ConnectionId}");
+            AddClient();
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Debug.WriteLine($"Disconnected: Client = {Context.ConnectionId}");
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            Debug.WriteLine($"Reconnected: Client = {Context.ConnectionId}");
+            RemoveClient();
+            return base.OnReconnected();
+        }
+    }
+}
